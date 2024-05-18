4764fdbd1c91b34a851af4c48a5c00072bd9a038
incognitotgt <tgt@incognitotgt.me>
2024-05-18 12:04:59 -0400

    format+lint

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,15 +3,15 @@ import { auth } from "@/lib/auth";
 import { db, user as userSchema } from "@/lib/drizzle/db";
 import { eq } from "drizzle-orm";
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const userSession = await auth();
-  const [dbUser] = await db
-    .select()
-    .from(userSchema)
-    .where(eq(userSchema.email, userSession?.user?.email as string));
-  return (
-    <main className="h-[90vh]">
-      <Navigation dbUser={dbUser} session={userSession} />
-      {children}
-    </main>
-  );
+	const userSession = await auth();
+	const [dbUser] = await db
+		.select()
+		.from(userSchema)
+		.where(eq(userSchema.email, userSession?.user?.email as string));
+	return (
+		<main className="h-[90vh]">
+			<Navigation dbUser={dbUser} session={userSession} />
+			{children}
+		</main>
+	);
 }
--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -4,27 +4,27 @@ import { Mail } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function EmailVerify() {
-  const session = await auth();
-  if (session) {
-    redirect("/");
-  }
-  return (
-    <>
-      <CardTitle className="mx-auto mb-2 flex flex-col items-center justify-center">
-        <Mail className="mb-10 h-12 w-12" />
-        Verify your email
-      </CardTitle>
-      <CardContent>
-        <p className="text-center">
-          We have sent a verification email to your email address. Please check your inbox and click on the link to
-          verify your email.
-        </p>
-      </CardContent>
-      <CardFooter>
-        <p className="text-center text-sm text-muted-foreground">
-          If you haven&apos;t received the email, please check your spam folder.
-        </p>
-      </CardFooter>
-    </>
-  );
+	const session = await auth();
+	if (session) {
+		redirect("/");
+	}
+	return (
+		<>
+			<CardTitle className="mx-auto mb-2 flex flex-col items-center justify-center">
+				<Mail className="mb-10 h-12 w-12" />
+				Verify your email
+			</CardTitle>
+			<CardContent>
+				<p className="text-center">
+					We have sent a verification email to your email address. Please check your inbox and click on the link to
+					verify your email.
+				</p>
+			</CardContent>
+			<CardFooter>
+				<p className="text-center text-sm text-muted-foreground">
+					If you haven&apos;t received the email, please check your spam folder.
+				</p>
+			</CardFooter>
+		</>
+	);
 }
