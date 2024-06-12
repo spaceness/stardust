54e3fc270557789135a244b3792c62a36eb95d97
rare1k <hi@rare1k.dev>
2024-06-12 11:01:17 -0400

    fix: no https

--- pull.sh
+++ pull.sh
@@ -1,9 +1,9 @@
 echo Pulling stardust containers...
 echo "" > /tmp/docker.log # overwrite if exists, create it if it doesnt
-sudo docker pull https://ghcr.io/spaceness/debian >> /tmp/docker.log
-sudo docker pull https://ghcr.io/spaceness/chromium >> /tmp/docker.log
-sudo docker pull https://ghcr.io/spaceness/firefox >> /tmp/docker.log
-sudo docker pull https://ghcr.io/spaceness/pinball >> /tmp/docker.log
-sudo docker pull https://ghcr.io/spaceness/debian-kde >> /tmp/docker.log
-sudo docker pull https://ghcr.io/spaceness/gimp >> /tmp/docker.log
+sudo docker pull ghcr.io/spaceness/debian >> /tmp/docker.log
+sudo docker pull ghcr.io/spaceness/chromium >> /tmp/docker.log
+sudo docker pull ghcr.io/spaceness/firefox >> /tmp/docker.log
+sudo docker pull ghcr.io/spaceness/pinball >> /tmp/docker.log
+sudo docker pull ghcr.io/spaceness/debian-kde >> /tmp/docker.log
+sudo docker pull ghcr.io/spaceness/gimp >> /tmp/docker.log
 echo Done!
