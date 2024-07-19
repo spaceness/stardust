7611eb22af61488289725a6caec7b22e81b3ff12
incognitotgt <tgt@incognitotgt.me>
2024-07-19 19:42:57 -0400

    fix(docker/v0.9) why is dockerode being inited with an iife

--- src/lib/docker.ts
+++ src/lib/docker.ts
@@ -1,12 +1,10 @@
 import Dockerode from "dockerode";
 import { getConfig } from "./config";
 const { docker: config } = getConfig();
-const docker = new Dockerode(
-	(() => ({
-		socketPath: !config.type || config.type === "socket" ? config.socket || "/var/run/docker.sock" : undefined,
-		host: config.type === "http" ? config.host : undefined,
-		port: config.type === "http" ? config.port : undefined,
-		protocol: config.type === "http" ? "http" : undefined,
-	}))(),
-);
+const docker = new Dockerode({
+	socketPath: !config.type || config.type === "socket" ? config.socket || "/var/run/docker.sock" : undefined,
+	host: config.type === "http" ? config.host : undefined,
+	port: config.type === "http" ? config.port : undefined,
+	protocol: config.type === "http" ? "http" : undefined,
+});
 export default docker;
